7553ca27297cfcb3326736c7306517c4ef3f2958
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-03-05 13:25:57 +0530

    [Aditya] Add . UC-2 feature Restrict the pinCode From taking Alphabets or Special Characters at Start

--- PinCodeValidator.js
+++ PinCodeValidator.js
@@ -1,7 +1,8 @@
-//validate pincode
-const regex = /^[0-9]{6}$/;
+//Improved the privious One To more Understandable
+const regex = /^[0-9][0-9]{5}$/;
 //function to validate pin code
 function validate(pincode){
     return regex.test(pincode);
 }
 console.log(validate(400088));
+console.log(validate("A40088"));
